db5674a3d9d39df8d906b15369b1d26f1a691b44
Iceman <Zachary.Draper85@gmail.com>
2019-05-30 11:33:58 -0600

    working on getting guess's to go down and game to reset when word is guessed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,38 +9,30 @@ console.log(word);
 let lettersGuessed = [];
 let wins = 0;
 let remainingGuess = 10;
-let remainingLetters = word.length;
+//let remainingLetters = word.length;
 //answerArray to put the lines for word on screen
 let answerArray = [];
 
 //Possibly the start of game
-for (let i = 0; i < word.length; i++) {
-    answerArray[i] = ("_");
-    console.log(answerArray);
-    document.getElementById('answerArray').textContent = answerArray.join(' ');
-    document.getElementById('guessLeft').textContent = remainingGuess;
-    document.getElementById('wins').textContent = wins;
-
-}
-keyEvent();
-guessInWord();
-while (answerArray != word || remainingGuess >= 0) {
-    remainingGuess--;
-}
-
-function guessInWord() {
+function startGame() {
     for (let i = 0; i < word.length; i++) {
-        if (remainingLetters === answerArray[i]) {
+        answerArray[i] = ("_");
+        console.log(answerArray);
+        document.getElementById('answerArray').textContent = answerArray.join(' ');
+        document.getElementById('guessLeft').textContent = remainingGuess;
+        document.getElementById('guessedLetters').textContent = lettersGuessed[i];
+        //document.getElementById('wins').textContent = wins;
+        word[i];
 
-            wins++
-        }
     }
 }
+startGame();
+keyEvent();
 
 function keyEvent() {
     document.onkeyup = function(e) {
-        lettersGuessed.push(e.key);
-        //console.log(lettersGuessed);
+        lettersGuessed.push(e.key)
+            //console.log(lettersGuessed);
         for (let i = 0; i < word.length; i++) {
             if (lettersGuessed.includes(word[i])) {
                 answerArray[i] = word[i];
@@ -49,9 +41,19 @@ function keyEvent() {
         }
         document.getElementById('answerArray').textContent = answerArray.join(' ');
         document.getElementById('guessedLetters').textContent = lettersGuessed.join(', ');
+        if (answerArray.join('') === word) {
+            alert("Winner!!! " + 'Word is ' + word)
+            wins++;
+        }
+        document.getElementById('wins').textContent = wins;
+        startGame();
     }
 
+    remainingGuess - lettersGuessed;
 }
+
+//function that regenerates the word and resets everything but the wins
+
 /*
 document.addEventListener('keyup', event => {
     //console.log('event.key', event.key);
@@ -65,40 +67,7 @@ document.addEventListener('keyup', event => {
     }
 });
 
-// find letter in word
-let letter = [];
 
-
-
-function isInWord(letter) {
-    document.getElementById('answerArray').textContent = letter.join(' ');
-    return word.includes(letter);
-}
-
-
-captures key's pressed and wether the key pressed is in the word or not
-document.addEventListener('keyup', event => {
-    //console.log('event.key', event.key);
-    if (isInWord(event.key, letter)) {
-        console.log(event.key + ' is in word: ' + letter);
-        document.getElementById('answerArray').textContent = answerArray = letter;
-        return answerArray.includes(letter);
-
-    } else {
-        // list wrong guesses
-        console.log(event.key + ' is NOT in word: ' + word);
-    }
-});
-
-// find letter in word
-let letter = [];
-
-
-//find letter in word
-
-function isInWord(letter) {
-    return word.includes(letter);
-}*/
 
 //I just cant figure out how to make it all work together. I think i need another array to put the letters that they have
 //pressed, if the push a correct letter it needs to go to the current word line and if it is not in the word it needs to 
